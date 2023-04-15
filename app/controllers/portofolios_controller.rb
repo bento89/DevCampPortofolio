30d1cc7263a77d1596e01514766fa0c5d22860ef
mof-devops <info.ifmisu@gmail.com>
2023-04-15 13:42:24 +0900

    add portofolio controller

--- app/controllers/portofolios_controller.rb
+++ app/controllers/portofolios_controller.rb
@@ -1,2 +1,23 @@
 class PortofoliosController < ApplicationController
+    def index
+        @portofolio_items = Portofolio.all
+    end
+
+    def new 
+        @portofolio_item = Portofolio.new
+    end
+
+    def create
+        @portofolio_item = Portofolio.new(params.require(:portofolio).permit(:title,:subtitle, :body))
+    
+        respond_to do |format|
+          if @portofolio_item.save
+            format.html { redirect_to portofolios_path(@portofolio_item), notice: "Portofolio was successfully created." }
+            format.json { render :show, status: :created, location: @portofolio_item }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @portofolio_item.errors, status: :unprocessable_entity }
+          end
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-Rails.application.routes.draw do
-  resources :blogs
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
-=======
 Rails.application.routes.draw do
   resources :portofolios
   get 'pages/home'
@@ -18,4 +9,3 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
->>>>>>> resource-generator
